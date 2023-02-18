b26794e8c35aacb77258b2172fa1b94e4452e0e5
BaronNova <125141681+BaronNova@users.noreply.github.com>
2023-02-18 18:58:42 +0300

    v1.0
    наконец то хоть что то есть

--- pingpong.py
+++ pingpong.py
@@ -8,26 +8,60 @@ FPS = 60
 
 windx = 700
 windy = 500
-FPS = 30
-lost = 0
-win = 0
-overheat = 0
-health = 3
-
 
 window = display.set_mode((windx, windy))
 window.fill(background)
 
-bullets = sprite.Group()
-
 playing = True
 finsh = False
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 420:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed
+
+player = Player('platform.png', windx-60, windy/2, 10)
+player2 = Player('platform.png', windx-690, windy/2, 10)
+balls = Player('ball.png', windx/2, windy/2, 10)
+
 
 
 while playing:
     for e in event.get():
         if e.type == QUIT:
             playing = False
+
+    if not finsh:
+        window.fill(background)
+        player.update_l()
+        player2.update_r()
+
+
+        balls.reset()
+        balls.reset()
+        player.reset()
+        player2.reset()
+        player.update()
+        player2.update()
     display.update()
     clock.tick(FPS)
-
